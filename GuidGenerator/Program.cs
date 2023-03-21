dea518a6cbdcac9cb0fdc04a28d213adfef0c703
Ben Nelson <binelson13@gmail.com>
2023-03-20 22:12:59 -0500

    Adding simple GUID generator

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GuidGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Guid.NewGuid());
+        }
+    }
+}
